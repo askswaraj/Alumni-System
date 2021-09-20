44fbb3ac019b4ff612aeb44b6bda89eb4a065858
Abhishek Swaraj <abhishektheswaraj@hotmail.com>
2021-09-20 16:11:26 +0530

    Test1

--- index.js
+++ index.js
@@ -31,13 +31,13 @@ app.use('/',postRouter);
 if(process.env.NODE_ENV=='production')
 {
 	app.use(express.static('client/build'));
-	app.get('*',(req,res)=>{
+	app.get("*",(req,res)=>{
 		res.sendFile(path.resolve(__dirname,'client','build','index.html'));
 	})
 }
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
+// app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+// });
 const port=process.env.PORT || 8080;
 app.listen(port,(req,res)=>{
 	console.log('server is listening on port'+port);
